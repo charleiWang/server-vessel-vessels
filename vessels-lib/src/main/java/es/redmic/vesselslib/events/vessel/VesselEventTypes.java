d7183dab742b160ba8db12a98501f1ce702b01ca
Noel Alonso <noel@redmic.es>
2018-08-14 14:19:03 +0100

    Elimina eventos no usados
    Se utiliza los mismos eventos de confirmación y fallo que en la
    edición total

--- vessels-lib/src/main/java/es/redmic/vesselslib/events/vessel/VesselEventTypes.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/events/vessel/VesselEventTypes.java
@@ -6,17 +6,11 @@ public abstract class VesselEventTypes extends EventTypes {
 
 	public static String
 	// @formatter:off
-		//UPDATE
-		UPDATE_VESSELTYPE = "UPDATE_VESSELTYPE",
-		UPDATE_VESSELTYPE_CONFIRMED = "UPDATE_VESSELTYPE_CONFIRMED",
-		VESSELTYPE_UPDATED = "VESSELTYPE_UPDATED",
-		UPDATE_VESSELTYPE_FAILED = "UPDATE_VESSELTYPE_FAILED",
-		UPDATE_VESSELTYPE_CANCELLED = "UPDATE_VESSELTYPE_CANCELLED";
+		UPDATE_VESSELTYPE = "UPDATE_VESSELTYPE";
 	//@formatter:on
 
 	public static boolean isLocked(String eventType) {
 
-		return (!(eventType.equals(VESSELTYPE_UPDATED.toString())
-				|| eventType.equals(UPDATE_VESSELTYPE_CANCELLED.toString())) && EventTypes.isLocked(eventType));
+		return EventTypes.isLocked(eventType);
 	}
 }
