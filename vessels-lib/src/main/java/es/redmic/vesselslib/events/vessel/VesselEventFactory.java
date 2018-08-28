a9dfa03135463293b14c38b10fbd8ddfd6315199
Noel Alonso <noel@redmic.es>
2018-08-28 11:22:04 +0100

    Completa todos los tipos en la factoría + tests

--- vessels-lib/src/main/java/es/redmic/vesselslib/events/vessel/VesselEventFactory.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/events/vessel/VesselEventFactory.java
@@ -6,82 +6,185 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import es.redmic.brokerlib.avro.common.Event;
+import es.redmic.brokerlib.avro.common.EventError;
+import es.redmic.exception.common.ExceptionType;
+import es.redmic.exception.common.InternalException;
+import es.redmic.vesselslib.dto.vessel.VesselDTO;
+import es.redmic.vesselslib.events.vessel.common.VesselCancelledEvent;
+import es.redmic.vesselslib.events.vessel.common.VesselEvent;
+import es.redmic.vesselslib.events.vessel.create.CreateVesselCancelledEvent;
 import es.redmic.vesselslib.events.vessel.create.CreateVesselFailedEvent;
+import es.redmic.vesselslib.events.vessel.create.VesselCreatedEvent;
+import es.redmic.vesselslib.events.vessel.delete.DeleteVesselCancelledEvent;
+import es.redmic.vesselslib.events.vessel.delete.DeleteVesselCheckFailedEvent;
+import es.redmic.vesselslib.events.vessel.delete.DeleteVesselCheckedEvent;
+import es.redmic.vesselslib.events.vessel.delete.DeleteVesselEvent;
 import es.redmic.vesselslib.events.vessel.delete.DeleteVesselFailedEvent;
+import es.redmic.vesselslib.events.vessel.delete.VesselDeletedEvent;
+import es.redmic.vesselslib.events.vessel.partialupdate.vesseltype.UpdateVesselTypeInVesselEvent;
+import es.redmic.vesselslib.events.vessel.update.UpdateVesselCancelledEvent;
 import es.redmic.vesselslib.events.vessel.update.UpdateVesselFailedEvent;
+import es.redmic.vesselslib.events.vessel.update.VesselUpdatedEvent;
 import es.redmic.vesselslib.events.vesseltype.VesselTypeEventTypes;
+import es.redmic.vesselslib.events.vesseltype.common.VesselTypeEvent;
 
 public class VesselEventFactory {
 
 	private static Logger logger = LogManager.getLogger();
 
+	public static Event getEvent(Event source, String type) {
+
+		if (type.equals(VesselEventTypes.DELETE)) {
+
+			logger.info("Creando evento DeleteVesselEvent para: " + source.getAggregateId());
+
+			return new DeleteVesselEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.DELETE_CHECKED)) {
+
+			logger.info("Creando evento DeleteVesselCheckedEvent para: " + source.getAggregateId());
+
+			return new DeleteVesselCheckedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.DELETED)) {
+
+			logger.info("Creando evento VesselDeletedEvent para: " + source.getAggregateId());
+
+			return new VesselDeletedEvent().buildFrom(source);
+		}
+
+		logger.error("Tipo de evento no soportado");
+		throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+	}
+
+	//////////////
+
+	public static Event getEvent(Event source, String type, VesselDTO vessel) {
+
+		VesselEvent successfulEvent = null;
+
+		if (type.equals(VesselEventTypes.CREATED)) {
+
+			logger.info("Creando evento VesselCreatedEvent para: " + source.getAggregateId());
+			successfulEvent = new VesselCreatedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.UPDATED)) {
+
+			logger.info("Creando evento VesselUpdatedEvent para: " + source.getAggregateId());
+			successfulEvent = new VesselUpdatedEvent().buildFrom(source);
+		}
+
+		if (successfulEvent != null) {
+			successfulEvent.setVessel(vessel);
+			return successfulEvent;
+		} else {
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+		}
+	}
+
+	//////////////
+
+	public static Event getEvent(Event source, Event trigger, String type) {
+
+		if (type.equals(VesselEventTypes.UPDATE_VESSELTYPE)) {
+
+			logger.info("Creando evento UpdateVesselTypeInVesselEvent para: " + source.getAggregateId());
+
+			UpdateVesselTypeInVesselEvent requestEvent = new UpdateVesselTypeInVesselEvent();
+			requestEvent.setAggregateId(source.getAggregateId());
+			requestEvent.setUserId(trigger.getUserId());
+			requestEvent.setVersion(source.getVersion() + 1);
+			requestEvent.setVesselType(((VesselTypeEvent) trigger).getVesselType());
+			return requestEvent;
+		}
+
+		logger.error("Tipo de evento no soportado");
+		throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+	}
+
+	/////////////////
+
 	public static Event getEvent(Event source, String type, String exceptionType,
 			Map<String, String> exceptionArguments) {
+
+		EventError failedEvent = null;
 
 		if (type.equals(VesselTypeEventTypes.CREATE_FAILED)) {
 
 			logger.info("No se pudo crear Vessel en la vista");
-			CreateVesselFailedEvent failedEvent = new CreateVesselFailedEvent().buildFrom(source);
-			failedEvent.setExceptionType(exceptionType);
-			failedEvent.setArguments(exceptionArguments);
-
-			return failedEvent;
+			failedEvent = new CreateVesselFailedEvent().buildFrom(source);
 		}
+
 		if (type.equals(VesselTypeEventTypes.UPDATE_FAILED)) {
 
 			logger.info("No se pudo modificar Vessel en la vista");
-			UpdateVesselFailedEvent failedEvent = new UpdateVesselFailedEvent().buildFrom(source);
-			failedEvent.setExceptionType(exceptionType);
-			failedEvent.setArguments(exceptionArguments);
-
-			return failedEvent;
+			failedEvent = new UpdateVesselFailedEvent().buildFrom(source);
 		}
+
 		if (type.equals(VesselTypeEventTypes.DELETE_FAILED)) {
 
 			logger.info("No se pudo eliminar Vessel de la vista");
-			DeleteVesselFailedEvent failedEvent = new DeleteVesselFailedEvent().buildFrom(source);
+			failedEvent = new DeleteVesselFailedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.DELETE_CHECK_FAILED)) {
+
+			logger.info("Checkeo de eliminación fallido, el item está referenciado");
+			failedEvent = new DeleteVesselCheckFailedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.CREATE_CANCELLED)) {
+
+			logger.info("Enviando evento CreateVesselCancelledEvent para: " + source.getAggregateId());
+			failedEvent = new CreateVesselCancelledEvent().buildFrom(source);
+		}
+
+		if (failedEvent != null) {
+
 			failedEvent.setExceptionType(exceptionType);
 			failedEvent.setArguments(exceptionArguments);
-
 			return failedEvent;
+
+		} else {
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
 		}
-		return null;
 	}
 
-	public static Event getEvent(Event source, String type) {
-		return null;
-	}
+	////////////////////
 
-	/*-public static CreateVesselFailedEvent getCreateVesselFailedEvent(Event event, String exceptionType,
+	public static Event getEvent(Event source, String type, VesselDTO vessel, String exceptionType,
 			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo crear Vessel en la vista");
-		CreateVesselFailedEvent failedEvent = new CreateVesselFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
+
+		VesselCancelledEvent cancelledEvent = null;
+
+		if (type.equals(VesselEventTypes.UPDATE_CANCELLED)) {
+
+			logger.info("Creando evento UpdateVesselCancelledEvent para: " + source.getAggregateId());
+			cancelledEvent = new UpdateVesselCancelledEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselEventTypes.DELETE_CANCELLED)) {
+
+			logger.info("Creando evento DeleteVesselCancelledEvent para: " + source.getAggregateId());
+			cancelledEvent = new DeleteVesselCancelledEvent().buildFrom(source);
+		}
+
+		if (cancelledEvent != null) {
+
+			cancelledEvent.setVessel(vessel);
+			cancelledEvent.setExceptionType(exceptionType);
+			cancelledEvent.setArguments(exceptionArguments);
+			return cancelledEvent;
+
+		} else {
+
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+		}
 	}
-	
-	public static UpdateVesselFailedEvent getUpdateVesselFailedEvent(Event event, String exceptionType,
-			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo modificar Vessel en la vista");
-		UpdateVesselFailedEvent failedEvent = new UpdateVesselFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
-	}
-	
-	public static DeleteVesselFailedEvent getDeleteVesselFailedEvent(Event event, String exceptionType,
-			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo eliminar Vessel de la vista");
-		DeleteVesselFailedEvent failedEvent = new DeleteVesselFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
-	}-*/
 }
--- vessels-lib/src/main/java/es/redmic/vesselslib/events/vesseltype/VesselTypeEventFactory.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/events/vesseltype/VesselTypeEventFactory.java
@@ -6,81 +6,149 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import es.redmic.brokerlib.avro.common.Event;
+import es.redmic.brokerlib.avro.common.EventError;
+import es.redmic.exception.common.ExceptionType;
+import es.redmic.exception.common.InternalException;
+import es.redmic.vesselslib.dto.vesseltype.VesselTypeDTO;
+import es.redmic.vesselslib.events.vesseltype.common.VesselTypeCancelledEvent;
+import es.redmic.vesselslib.events.vesseltype.common.VesselTypeEvent;
+import es.redmic.vesselslib.events.vesseltype.create.CreateVesselTypeCancelledEvent;
 import es.redmic.vesselslib.events.vesseltype.create.CreateVesselTypeFailedEvent;
+import es.redmic.vesselslib.events.vesseltype.create.VesselTypeCreatedEvent;
+import es.redmic.vesselslib.events.vesseltype.delete.DeleteVesselTypeCancelledEvent;
+import es.redmic.vesselslib.events.vesseltype.delete.DeleteVesselTypeCheckFailedEvent;
+import es.redmic.vesselslib.events.vesseltype.delete.DeleteVesselTypeCheckedEvent;
+import es.redmic.vesselslib.events.vesseltype.delete.DeleteVesselTypeEvent;
 import es.redmic.vesselslib.events.vesseltype.delete.DeleteVesselTypeFailedEvent;
+import es.redmic.vesselslib.events.vesseltype.delete.VesselTypeDeletedEvent;
+import es.redmic.vesselslib.events.vesseltype.update.UpdateVesselTypeCancelledEvent;
 import es.redmic.vesselslib.events.vesseltype.update.UpdateVesselTypeFailedEvent;
+import es.redmic.vesselslib.events.vesseltype.update.VesselTypeUpdatedEvent;
 
 public class VesselTypeEventFactory {
 
 	private static Logger logger = LogManager.getLogger();
 
+	public static Event getEvent(Event source, String type) {
+
+		if (type.equals(VesselTypeEventTypes.DELETE)) {
+
+			logger.info("Creando evento DeleteVesselTypeEvent para: " + source.getAggregateId());
+			return new DeleteVesselTypeEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.DELETE_CHECKED)) {
+
+			logger.info("Creando evento DeleteVesselTypeCheckedEvent para: " + source.getAggregateId());
+			return new DeleteVesselTypeCheckedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.DELETED)) {
+
+			logger.info("Creando evento VesselTypeDeletedEvent para: " + source.getAggregateId());
+			return new VesselTypeDeletedEvent().buildFrom(source);
+		}
+
+		logger.error("Tipo de evento no soportado");
+		throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+	}
+
+	public static Event getEvent(Event source, String type, VesselTypeDTO vesselType) {
+
+		VesselTypeEvent successfulEvent = null;
+
+		if (type.equals(VesselTypeEventTypes.CREATED)) {
+			logger.info("Creando evento VesselTypeCreatedEvent para: " + source.getAggregateId());
+			successfulEvent = new VesselTypeCreatedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.UPDATED)) {
+			logger.info("Creando evento VesselTypeUpdatedEvent para: " + source.getAggregateId());
+			successfulEvent = new VesselTypeUpdatedEvent().buildFrom(source);
+		}
+
+		if (successfulEvent != null) {
+			successfulEvent.setVesselType(vesselType);
+			return successfulEvent;
+		} else {
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+		}
+	}
+
 	public static Event getEvent(Event source, String type, String exceptionType,
 			Map<String, String> exceptionArguments) {
+
+		EventError failedEvent = null;
 
 		if (type.equals(VesselTypeEventTypes.CREATE_FAILED)) {
 
 			logger.info("No se pudo crear Vessel type en la vista");
-			CreateVesselTypeFailedEvent failedEvent = new CreateVesselTypeFailedEvent().buildFrom(source);
-			failedEvent.setExceptionType(exceptionType);
-			failedEvent.setArguments(exceptionArguments);
-
-			return failedEvent;
+			failedEvent = new CreateVesselTypeFailedEvent().buildFrom(source);
 		}
 		if (type.equals(VesselTypeEventTypes.UPDATE_FAILED)) {
 
 			logger.info("No se pudo modificar Vessel type en la vista");
-			UpdateVesselTypeFailedEvent failedEvent = new UpdateVesselTypeFailedEvent().buildFrom(source);
-			failedEvent.setExceptionType(exceptionType);
-			failedEvent.setArguments(exceptionArguments);
-
-			return failedEvent;
+			failedEvent = new UpdateVesselTypeFailedEvent().buildFrom(source);
 		}
 		if (type.equals(VesselTypeEventTypes.DELETE_FAILED)) {
 
 			logger.info("No se pudo eliminar Vessel type de la vista");
-			DeleteVesselTypeFailedEvent failedEvent = new DeleteVesselTypeFailedEvent().buildFrom(source);
+			failedEvent = new DeleteVesselTypeFailedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.DELETE_CHECK_FAILED)) {
+
+			logger.info("Checkeo de eliminación fallido, el item está referenciado");
+			failedEvent = new DeleteVesselTypeCheckFailedEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.CREATE_CANCELLED)) {
+
+			logger.info("Enviando evento CreateVesselTypeCancelledEvent para: " + source.getAggregateId());
+			failedEvent = new CreateVesselTypeCancelledEvent().buildFrom(source);
+		}
+
+		if (failedEvent != null) {
+
 			failedEvent.setExceptionType(exceptionType);
 			failedEvent.setArguments(exceptionArguments);
-
 			return failedEvent;
+
+		} else {
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
 		}
-		return null;
 	}
 
-	public static Event getEvent(Event source, String type) {
-		return null;
-	}
+	public static Event getEvent(Event source, String type, VesselTypeDTO vesselType, String exceptionType,
+			Map<String, String> exceptionArguments) {
 
-	/*-public static CreateVesselTypeFailedEvent getCreateVesselTypeFailedEvent(Event event, String exceptionType,
-			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo crear Vessel type en la vista");
-		CreateVesselTypeFailedEvent failedEvent = new CreateVesselTypeFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
+		VesselTypeCancelledEvent cancelledEvent = null;
+
+		if (type.equals(VesselTypeEventTypes.UPDATE_CANCELLED)) {
+
+			logger.info("Creando evento UpdateVesselTypeCancelledEvent para: " + source.getAggregateId());
+			cancelledEvent = new UpdateVesselTypeCancelledEvent().buildFrom(source);
+		}
+
+		if (type.equals(VesselTypeEventTypes.DELETE_CANCELLED)) {
+
+			logger.info("Creando evento DeleteVesselTypeCancelledEvent para: " + source.getAggregateId());
+			cancelledEvent = new DeleteVesselTypeCancelledEvent().buildFrom(source);
+		}
+
+		if (cancelledEvent != null) {
+
+			cancelledEvent.setVesselType(vesselType);
+			cancelledEvent.setExceptionType(exceptionType);
+			cancelledEvent.setArguments(exceptionArguments);
+			return cancelledEvent;
+
+		} else {
+
+			logger.error("Tipo de evento no soportado");
+			throw new InternalException(ExceptionType.INTERNAL_EXCEPTION);
+		}
 	}
-	
-	public static UpdateVesselTypeFailedEvent getUpdateVesselTypeFailedEvent(Event event, String exceptionType,
-			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo modificar Vessel type en la vista");
-		UpdateVesselTypeFailedEvent failedEvent = new UpdateVesselTypeFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
-	}
-	
-	public static DeleteVesselTypeFailedEvent getDeleteVesselTypeFailedEvent(Event event, String exceptionType,
-			Map<String, String> exceptionArguments) {
-	
-		logger.info("No se pudo eliminar Vessel type de la vista");
-		DeleteVesselTypeFailedEvent failedEvent = new DeleteVesselTypeFailedEvent().buildFrom(event);
-		failedEvent.setExceptionType(exceptionType);
-		failedEvent.setArguments(exceptionArguments);
-	
-		return failedEvent;
-	}-*/
 }
