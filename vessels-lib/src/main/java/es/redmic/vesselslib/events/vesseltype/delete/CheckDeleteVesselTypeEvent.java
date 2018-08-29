83f1f8efed653415a47c3de4ee427849c549bda9
Noel Alonso <noel@redmic.es>
2018-08-29 10:24:17 +0100

    Añade seteo de sessionId
    Solo necesario en eventos que comienzan el ciclo

--- vessels-lib/src/main/java/es/redmic/vesselslib/events/vesseltype/delete/CheckDeleteVesselTypeEvent.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/events/vesseltype/delete/CheckDeleteVesselTypeEvent.java
@@ -1,5 +1,7 @@
 package es.redmic.vesselslib.events.vesseltype.delete;
 
+import java.util.UUID;
+
 import org.apache.avro.Schema;
 
 import es.redmic.brokerlib.avro.common.SimpleEvent;
@@ -19,6 +21,7 @@ public class CheckDeleteVesselTypeEvent extends SimpleEvent {
 
 	public CheckDeleteVesselTypeEvent() {
 		super(type);
+		setSessionId(UUID.randomUUID().toString());
 	}
 
 	@Override
