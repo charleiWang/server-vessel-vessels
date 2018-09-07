885a8860f9c58150c974c7a32d93cbfb8e9447fa
Noel Alonso <noel@redmic.es>
2018-09-07 10:04:50 +0100

    Elimina código no usado

--- vessels-commands/src/main/java/es/redmic/vesselscommands/controller/VesselController.java
+++ vessels-commands/src/main/java/es/redmic/vesselscommands/controller/VesselController.java
@@ -1,9 +1,5 @@
 package es.redmic.vesselscommands.controller;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
@@ -17,9 +13,6 @@ import es.redmic.vesselslib.dto.vessel.VesselDTO;
 public class VesselController extends CommandController<VesselDTO> {
 
 	VesselCommandService service;
-
-	ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-	Validator validator = factory.getValidator();
 
 	@Autowired
 	public VesselController(VesselCommandService service) {
