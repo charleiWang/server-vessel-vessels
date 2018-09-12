26341116faf4a3bb1f51a5e88f3d608deb46ac55
Noel Alonso <noel@redmic.es>
2018-09-12 13:51:41 +0100

    Elimina etiqueta, activando autoconfiguración

--- vessels-view/src/main/java/es/redmic/vesselsview/VesselsViewApplication.java
+++ vessels-view/src/main/java/es/redmic/vesselsview/VesselsViewApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import es.redmic.restlib.config.ResourceBundleMessageSource;
 
 @SpringBootApplication
-@EnableWebMvc
 @ComponentScan({ "es.redmic.vesselsview", "es.redmic.viewlib.common.mapper.es2dto", "es.redmic.elasticsearchlib",
 		"es.redmic.restlib" })
 public class VesselsViewApplication {
