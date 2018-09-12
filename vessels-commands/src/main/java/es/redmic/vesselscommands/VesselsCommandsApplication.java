cb05d79f8819bace19ef5044d0d94c98e80b8f83
Noel Alonso <noel@redmic.es>
2018-09-12 13:54:35 +0100

    Elimina etiqueta, activando autoconfiguración

--- vessels-commands/src/main/java/es/redmic/vesselscommands/VesselsCommandsApplication.java
+++ vessels-commands/src/main/java/es/redmic/vesselscommands/VesselsCommandsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
 import com.fasterxml.jackson.databind.Module;
@@ -14,7 +13,6 @@ import es.redmic.commandslib.config.GenerateJsonSchemaScanBean;
 import es.redmic.restlib.config.ResourceBundleMessageSource;
 
 @SpringBootApplication
-@EnableWebMvc
 @ComponentScan({ "es.redmic.vesselscommands", "es.redmic.restlib", "es.redmic.commandslib",
 		"es.redmic.brokerlib.alert" })
 public class VesselsCommandsApplication {
