4d05f5aeb9a7ee0cb66ed506c9ddad81dea8f0a9
Noel Alonso <noel@redmic.es>
2018-09-19 14:36:33 +0100

    Reactiva mapper

--- vessels-view/src/main/java/es/redmic/vesselsview/mapper/vesseltracking/VesselTrackingPropertiesESMapper.java
+++ vessels-view/src/main/java/es/redmic/vesselsview/mapper/vesseltracking/VesselTrackingPropertiesESMapper.java
@@ -1,6 +1,12 @@
 package es.redmic.vesselsview.mapper.vesseltracking;
 
-/*-@Component
+import org.springframework.stereotype.Component;
+
+import es.redmic.vesselslib.dto.tracking.VesselTrackingPropertiesDTO;
+import es.redmic.vesselsview.model.vesseltracking.VesselTrackingProperties;
+import ma.glasnost.orika.CustomMapper;
+
+@Component
 public class VesselTrackingPropertiesESMapper
 		extends CustomMapper<VesselTrackingProperties, VesselTrackingPropertiesDTO> {
-}-*/
+}
