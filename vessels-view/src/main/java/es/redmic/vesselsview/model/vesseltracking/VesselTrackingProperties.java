f71e774cce5118e5ef4baad0f4becf9f2edbc6c5
Noel Alonso <noel@redmic.es>
2018-09-19 14:37:41 +0100

    Sobrescribe método equals

--- vessels-view/src/main/java/es/redmic/vesselsview/model/vesseltracking/VesselTrackingProperties.java
+++ vessels-view/src/main/java/es/redmic/vesselsview/model/vesseltracking/VesselTrackingProperties.java
@@ -97,4 +97,72 @@ public class VesselTrackingProperties extends Properties {
 	public void setEta(String eta) {
 		this.eta = eta;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cog == null) ? 0 : cog.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((dest == null) ? 0 : dest.hashCode());
+		result = prime * result + ((eta == null) ? 0 : eta.hashCode());
+		result = prime * result + ((heading == null) ? 0 : heading.hashCode());
+		result = prime * result + ((navStat == null) ? 0 : navStat.hashCode());
+		result = prime * result + ((sog == null) ? 0 : sog.hashCode());
+		result = prime * result + ((vessel == null) ? 0 : vessel.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VesselTrackingProperties other = (VesselTrackingProperties) obj;
+		if (cog == null) {
+			if (other.cog != null)
+				return false;
+		} else if (!cog.equals(other.cog))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (dest == null) {
+			if (other.dest != null)
+				return false;
+		} else if (!dest.equals(other.dest))
+			return false;
+		if (eta == null) {
+			if (other.eta != null)
+				return false;
+		} else if (!eta.equals(other.eta))
+			return false;
+		if (heading == null) {
+			if (other.heading != null)
+				return false;
+		} else if (!heading.equals(other.heading))
+			return false;
+		if (navStat == null) {
+			if (other.navStat != null)
+				return false;
+		} else if (!navStat.equals(other.navStat))
+			return false;
+		if (sog == null) {
+			if (other.sog != null)
+				return false;
+		} else if (!sog.equals(other.sog))
+			return false;
+		if (vessel == null) {
+			if (other.vessel != null)
+				return false;
+		} else if (!vessel.equals(other.vessel))
+			return false;
+		return true;
+	}
+
 }
