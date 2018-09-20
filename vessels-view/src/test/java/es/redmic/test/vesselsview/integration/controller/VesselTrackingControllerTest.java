d773c045c2f96bbe44c1c90ce8e1ff825389af64
Noel Alonso <noel@redmic.es>
2018-09-20 14:35:02 +0100

    Añade tests de integración

---
+++ vessels-view/src/test/java/es/redmic/test/vesselsview/integration/controller/VesselTrackingControllerTest.java
@@ -0,0 +1,259 @@
+package es.redmic.test.vesselsview.integration.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Point;
+
+import es.redmic.models.es.common.query.dto.DataQueryDTO;
+import es.redmic.models.es.common.query.dto.MgetDTO;
+import es.redmic.testutils.documentation.DocumentationViewBaseTest;
+import es.redmic.vesselsview.VesselsViewApplication;
+import es.redmic.vesselsview.model.vessel.Vessel;
+import es.redmic.vesselsview.model.vesseltracking.VesselTracking;
+import es.redmic.vesselsview.model.vesseltracking.VesselTrackingProperties;
+import es.redmic.vesselsview.model.vesseltype.VesselType;
+import es.redmic.vesselsview.repository.vesseltracking.VesselTrackingESRepository;
+
+@SpringBootTest(classes = { VesselsViewApplication.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = { "schema.registry.port=18181" })
+@DirtiesContext
+public class VesselTrackingControllerTest extends DocumentationViewBaseTest {
+
+	public final static String PREFIX = "vesseltracking-mmsi-tstamp-", MMSI = "1234", USER = "1";
+
+	@Value("${documentation.VESSELTRACKING_HOST}")
+	private String VESSELTRACKING_HOST;
+
+	@Value("${controller.mapping.vesseltracking}")
+	private String VESSELTRACKING_PATH;
+
+	@Value("${vesseltracking.activity.id}")
+	private String ACTIVITY_ID;
+
+	String url;
+
+	@Autowired
+	VesselTrackingESRepository repository;
+
+	VesselTracking vesselTracking = new VesselTracking();
+
+	@ClassRule
+	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1);
+
+	@PostConstruct
+	public void CreateVesselTrackingFromRestTestPostConstruct() throws Exception {
+
+		createSchemaRegistryRestApp(embeddedKafka.getZookeeperConnectionString(), embeddedKafka.getBrokersAsString());
+	}
+
+	@Override
+	@Before
+	public void setUp() {
+
+		vesselTracking = new VesselTracking();
+
+		Vessel vessel = new Vessel();
+		vessel.setId("vessel-mmsi-" + MMSI);
+		vessel.setMmsi(Integer.valueOf(MMSI));
+		vessel.setName("Avatar");
+		vessel.setImo(1234);
+		vessel.setBeam(30.2);
+		vessel.setLength(230.5);
+		vessel.setCallSign("23e2");
+		vessel.setInserted(DateTime.now());
+		vessel.setUpdated(DateTime.now());
+
+		VesselType vesselType = new VesselType();
+		vesselType.setCode("70");
+		vesselType.setId("vesseltype-code-" + "70");
+		vesselType.setName("Cargo, all ships of this type");
+		vesselType.setName_en("Cargo, all ships of this type");
+		vessel.setType(vesselType);
+
+		vesselTracking.setId(PREFIX + MMSI + "-" + new DateTime().getMillis());
+		vesselTracking.setUuid(UUID.randomUUID().toString());
+
+		Point geometry = JTSFactoryFinder.getGeometryFactory().createPoint(new Coordinate(44.56433, 37.94388));
+		vesselTracking.setGeometry(geometry);
+
+		VesselTrackingProperties properties = new VesselTrackingProperties();
+		vesselTracking.setProperties(properties);
+
+		properties.setActivity("r.1.8.22");
+		properties.setVessel(vessel);
+		properties.setDate(DateTime.now());
+
+		properties.setCog(23.3);
+		properties.setSog(23.3);
+		properties.setHeading(12);
+		properties.setNavStat(33);
+		properties.setDest("Santa Cruz de Tenerife");
+		properties.setEta("00:00 00:00");
+
+		properties.setInserted(DateTime.now());
+		properties.setUpdated(DateTime.now());
+
+		repository.save(vesselTracking);
+
+		url = VESSELTRACKING_PATH.replace("{activityId}", ACTIVITY_ID);
+
+		// @formatter:off
+
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(webApplicationContext)
+				.addFilters(springSecurityFilterChain)
+				.apply(documentationConfiguration(this.restDocumentation)
+						.uris().withScheme(SCHEME).withHost(VESSELTRACKING_HOST.replace("{activityId}", ACTIVITY_ID))
+							.withPort(PORT))
+				.alwaysDo(this.document).build();
+
+		// @formatter:on
+	}
+
+	@After
+	public void clean() {
+		repository.delete(vesselTracking.getId());
+	}
+
+	@Test
+	public void getVessel_Return200_WhenItemExist() throws Exception {
+
+		// @formatter:off
+		
+		this.mockMvc.perform(get(url + "/" + vesselTracking.getId()).accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.success", is(true)))
+			.andExpect(jsonPath("$.body", notNullValue()))
+			.andExpect(jsonPath("$.body.id", is(vesselTracking.getId())));
+		
+		// @formatter:on
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void searchVesselTrackingPost_Return200_WhenSearchIsCorrect() throws Exception {
+
+		DataQueryDTO dataQuery = new DataQueryDTO();
+		dataQuery.setSize(1);
+
+		// Se elimina accessibilityIds ya que no está permitido para usuarios
+		// no registrados
+		HashMap<String, Object> query = mapper.convertValue(dataQuery, HashMap.class);
+		query.remove("accessibilityIds");
+
+		// @formatter:off
+		
+		this.mockMvc
+				.perform(post(url + "/_search").content(mapper.writeValueAsString(query))
+					.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success", is(true)))
+				.andExpect(jsonPath("$.body.features", notNullValue()))
+				.andExpect(jsonPath("$.body.features[0]", notNullValue()))
+				.andExpect(jsonPath("$.body.features.length()", is(1)))
+					.andDo(getDataQueryFieldsDescriptor());
+		
+		// @formatter:on
+	}
+
+	@Test
+	public void mgetVesselTracking_Return200_WhenVesselTrackingExists() throws Exception {
+
+		MgetDTO mgetQuery = new MgetDTO();
+		mgetQuery.setIds(Arrays.asList(vesselTracking.getId()));
+		mgetQuery.setFields(Arrays.asList("id"));
+
+		// @formatter:off
+		
+		this.mockMvc
+			.perform(post(url + "/_mget").content(mapper.writeValueAsString(mgetQuery))
+					.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success", is(true)))
+				.andExpect(jsonPath("$.body.features", notNullValue()))
+				.andExpect(jsonPath("$.body.features[0]", notNullValue()))
+				.andExpect(jsonPath("$.body.features[0].id", is(vesselTracking.getId())))
+				.andExpect(jsonPath("$.body.features.length()", is(1)))
+					.andDo(getMgetRequestDescription());
+		
+		// @formatter:on
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void suggestVesselTrackingPost_Return200_WhenSuggestIsCorrect() throws Exception {
+
+		DataQueryDTO dataQuery = new DataQueryDTO();
+		dataQuery.setSize(1);
+		dataQuery.createSimpleQueryDTOFromSuggestQueryParams(new String[] { "properties.dest" },
+				vesselTracking.getProperties().getDest(), 1);
+
+		// Se elimina accessibilityIds ya que no está permitido para usuarios
+		// no registrados
+		HashMap<String, Object> query = mapper.convertValue(dataQuery, HashMap.class);
+		query.remove("accessibilityIds");
+
+		// @formatter:off
+		
+		this.mockMvc
+			.perform(post(url + "/_suggest").content(mapper.writeValueAsString(query))
+					.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success", is(true)))
+				.andExpect(jsonPath("$.body", notNullValue()))
+				.andExpect(jsonPath("$.body.length()", is(1))) 
+					.andDo(getDataQueryFieldsDescriptor());
+				
+		
+		// @formatter:on
+	}
+
+	@Test
+	public void getFilterSchema_Return200_WhenSchemaIsAvailable() throws Exception {
+
+		// @formatter:off
+		
+		this.mockMvc.perform(get(url + filterSchemaPath)
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.success", is(true)))
+			.andExpect(jsonPath("$.body", notNullValue()))
+			.andExpect(jsonPath("$.body.schema", notNullValue()))
+			.andExpect(jsonPath("$.body.schema.properties", notNullValue()))
+			.andExpect(jsonPath("$.body.schema.properties.postFilter", notNullValue()))
+			.andExpect(jsonPath("$.body.schema.properties.aggs", notNullValue()));
+			// TODO: aumentar el nivel de checkeo
+		// @formatter:on
+	}
+}
---
+++ vessels-view/src/test/java/es/redmic/test/vesselsview/integration/handler/VesselTrackingEventHandlerTest.java
@@ -0,0 +1,466 @@
+package es.redmic.test.vesselsview.integration.handler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.joda.time.DateTime;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Point;
+
+import es.redmic.brokerlib.avro.common.Event;
+import es.redmic.brokerlib.listener.SendListener;
+import es.redmic.exception.data.ItemNotFoundException;
+import es.redmic.models.es.geojson.wrapper.GeoHitWrapper;
+import es.redmic.testutils.documentation.DocumentationViewBaseTest;
+import es.redmic.vesselslib.dto.tracking.VesselTrackingDTO;
+import es.redmic.vesselslib.dto.tracking.VesselTrackingPropertiesDTO;
+import es.redmic.vesselslib.dto.vessel.VesselDTO;
+import es.redmic.vesselslib.dto.vesseltype.VesselTypeDTO;
+import es.redmic.vesselslib.events.vesseltracking.VesselTrackingEventTypes;
+import es.redmic.vesselslib.events.vesseltracking.create.CreateVesselTrackingConfirmedEvent;
+import es.redmic.vesselslib.events.vesseltracking.create.CreateVesselTrackingEvent;
+import es.redmic.vesselslib.events.vesseltracking.create.CreateVesselTrackingFailedEvent;
+import es.redmic.vesselslib.events.vesseltracking.delete.DeleteVesselTrackingConfirmedEvent;
+import es.redmic.vesselslib.events.vesseltracking.delete.DeleteVesselTrackingEvent;
+import es.redmic.vesselslib.events.vesseltracking.delete.DeleteVesselTrackingFailedEvent;
+import es.redmic.vesselslib.events.vesseltracking.partialupdate.vessel.UpdateVesselInVesselTrackingEvent;
+import es.redmic.vesselslib.events.vesseltracking.update.UpdateVesselTrackingConfirmedEvent;
+import es.redmic.vesselslib.events.vesseltracking.update.UpdateVesselTrackingEvent;
+import es.redmic.vesselslib.events.vesseltracking.update.UpdateVesselTrackingFailedEvent;
+import es.redmic.vesselsview.VesselsViewApplication;
+import es.redmic.vesselsview.model.vesseltracking.VesselTracking;
+import es.redmic.vesselsview.repository.vesseltracking.VesselTrackingESRepository;
+import es.redmic.viewlib.config.MapperScanBeanItfc;
+
+@SpringBootTest(classes = { VesselsViewApplication.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@KafkaListener(topics = "${broker.topic.vessel-tracking}", groupId = "test")
+@TestPropertySource(properties = { "schema.registry.port=18183" })
+@DirtiesContext
+@ActiveProfiles("test")
+public class VesselTrackingEventHandlerTest extends DocumentationViewBaseTest {
+
+	// @formatter:off
+
+	private final String USER_ID = "1",
+			PREFIX = "vesseltracking-mmsi-tstamp-",
+			MMSI = "1234",
+			_UUID = UUID.randomUUID().toString();
+	
+	// @formatter:on
+
+	Long TSTAMP = new DateTime().getMillis();
+
+	@Autowired
+	MapperScanBeanItfc mapper;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Autowired
+	private KafkaTemplate<String, Event> kafkaTemplate;
+
+	protected static BlockingQueue<Object> blockingQueue;
+
+	@Value("${broker.topic.vessel-tracking}")
+	private String VESSELTRACKING_TOPIC;
+
+	@Autowired
+	VesselTrackingESRepository repository;
+
+	@ClassRule
+	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1);
+
+	@PostConstruct
+	public void VesselTrackingEventHandlerTestPostConstruct() throws Exception {
+
+		createSchemaRegistryRestApp(embeddedKafka.getZookeeperConnectionString(), embeddedKafka.getBrokersAsString());
+	}
+
+	@BeforeClass
+	public static void setup() {
+
+		blockingQueue = new LinkedBlockingDeque<>();
+	}
+
+	@Override
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void sendVesselTrackingCreatedEvent_SaveItem_IfEventIsOk() throws Exception {
+
+		CreateVesselTrackingEvent event = getCreateVesselTrackingEvent();
+
+		repository.delete(event.getVesselTracking().getId());
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				event.getAggregateId(), event);
+		future.addCallback(new SendListener());
+
+		Event confirm = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+
+		GeoHitWrapper<?> item = repository.findById(event.getVesselTracking().getId());
+		assertNotNull(item.get_source());
+
+		// Se restablece el estado de la vista
+		repository.delete(event.getVesselTracking().getId());
+
+		assertNotNull(confirm);
+		assertEquals(VesselTrackingEventTypes.CREATE_CONFIRMED, confirm.getType());
+
+		VesselTracking vesselTracking = (VesselTracking) item.get_source();
+		assertEqualsVesselTracking(vesselTracking, event.getVesselTracking());
+	}
+
+	@Test
+	public void sendVesselTrackingUpdatedEvent_UpdateItem_IfEventIsOk() throws Exception {
+
+		UpdateVesselTrackingEvent event = getUpdateVesselTrackingEvent();
+
+		repository.save(mapper.getMapperFacade().map(event.getVesselTracking(), VesselTracking.class));
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				event.getAggregateId(), event);
+		future.addCallback(new SendListener());
+
+		Event confirm = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+
+		GeoHitWrapper<?> item = repository.findById(event.getVesselTracking().getId());
+		assertNotNull(item.get_source());
+
+		// Se restablece el estado de la vista
+		repository.delete(event.getVesselTracking().getId());
+
+		assertNotNull(confirm);
+		assertEquals(VesselTrackingEventTypes.UPDATE_CONFIRMED.toString(), confirm.getType());
+
+		VesselTracking vesselTracking = (VesselTracking) item.get_source();
+		assertEqualsVesselTracking(vesselTracking, event.getVesselTracking());
+	}
+
+	@Test
+	public void sendUpdateVesselInVesselTrackingEvent_UpdateItem_IfEventIsOk() throws Exception {
+
+		VesselTrackingDTO dto = getVesselTracking();
+
+		repository.save(mapper.getMapperFacade().map(dto, VesselTracking.class));
+
+		UpdateVesselInVesselTrackingEvent updateEvent = getUpdateVesselInVesselTrackingEvent();
+		updateEvent.setAggregateId(dto.getId());
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				updateEvent.getAggregateId(), updateEvent);
+		future.addCallback(new SendListener());
+
+		Event confirm = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+
+		GeoHitWrapper<?> item = repository.findById(dto.getId());
+		assertNotNull(item.get_source());
+
+		// Se restablece el estado de la vista
+		repository.delete(dto.getId());
+
+		assertNotNull(confirm);
+		assertEquals(VesselTrackingEventTypes.UPDATE_CONFIRMED.toString(), confirm.getType());
+
+		VesselTracking vesselTracking = (VesselTracking) item.get_source();
+		assertEqualsVesselTracking(vesselTracking, dto);
+	}
+
+	@Test(expected = ItemNotFoundException.class)
+	public void sendVesselTrackingDeleteEvent_DeleteItem_IfEventIsOk() throws Exception {
+
+		DeleteVesselTrackingEvent event = getDeleteVesselTrackingEvent();
+
+		repository.save(
+				mapper.getMapperFacade().map(getUpdateVesselTrackingEvent().getVesselTracking(), VesselTracking.class));
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				event.getAggregateId(), event);
+		future.addCallback(new SendListener());
+
+		Event confirm = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+		assertNotNull(confirm);
+		assertEquals(VesselTrackingEventTypes.DELETE_CONFIRMED.toString(), confirm.getType());
+
+		GeoHitWrapper<?> item = repository.findById(event.getAggregateId());
+		assertNull(item.get_source());
+	}
+
+	@Test
+	public void sendVesselTrackingCreatedEvent_PublishCreateVesselTrackingFailedEvent_IfNoConstraintsFulfilled()
+			throws Exception {
+
+		CreateVesselTrackingEvent event = getCreateVesselTrackingEvent();
+
+		repository.save(mapper.getMapperFacade().map(event.getVesselTracking(), VesselTracking.class));
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				event.getAggregateId(), event);
+		future.addCallback(new SendListener());
+
+		Event fail = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+
+		// Se restablece el estado de la vista
+		repository.delete(event.getVesselTracking().getId());
+
+		assertNotNull(fail);
+		assertEquals(VesselTrackingEventTypes.CREATE_FAILED.toString(), fail.getType());
+		CreateVesselTrackingFailedEvent createVesselTrackingFailedEvent = (CreateVesselTrackingFailedEvent) fail;
+
+		Map<String, String> arguments = createVesselTrackingFailedEvent.getArguments();
+		assertNotNull(arguments);
+
+		assertEquals(4, arguments.size());
+
+		assertNotNull(arguments.get("id"));
+		assertNotNull(arguments.get("uuid"));
+		assertNotNull(arguments.get("properties.vessel.mmsi"));
+		assertNotNull(arguments.get("properties.date"));
+	}
+
+	@Test
+	public void sendVesselTrackingUpdateEvent_PublishUpdateVesselTrackingFailedEvent_IfNoConstraintsFulfilled()
+			throws Exception {
+
+		UpdateVesselTrackingEvent event = getUpdateVesselTrackingEvent();
+
+		// @formatter:off
+
+		VesselTrackingDTO conflict = event.getVesselTracking(),
+				original = getVesselTracking();
+		
+		conflict.setId(original.getId()+"cpy");
+		conflict.setUuid(original.getUuid()+"cpy");
+		conflict.getProperties().getVessel().setMmsi(3456);
+		
+		// @formatter:on
+
+		// Guarda el que se va a modificar
+		repository.save(mapper.getMapperFacade().map(original, VesselTracking.class));
+
+		// Guarda el que va a entrar en conflicto
+		repository.save(mapper.getMapperFacade().map(conflict, VesselTracking.class));
+
+		// Edita el mmsi del que se va a modificar para entrar en conflicto
+		original.getProperties().getVessel().setMmsi(conflict.getProperties().getVessel().getMmsi());
+		original.getProperties().setDate(conflict.getProperties().getDate());
+
+		event.setVesselTracking(original);
+
+		ListenableFuture<SendResult<String, Event>> future = kafkaTemplate.send(VESSELTRACKING_TOPIC,
+				event.getAggregateId(), event);
+		future.addCallback(new SendListener());
+
+		Event fail = (Event) blockingQueue.poll(50, TimeUnit.SECONDS);
+
+		// Se restablece el estado de la vista
+		repository.delete(original.getId());
+		repository.delete(conflict.getId());
+
+		assertNotNull(fail);
+		assertEquals(VesselTrackingEventTypes.UPDATE_FAILED.toString(), fail.getType());
+
+		UpdateVesselTrackingFailedEvent createVesselTrackingFailedEvent = (UpdateVesselTrackingFailedEvent) fail;
+
+		Map<String, String> arguments = createVesselTrackingFailedEvent.getArguments();
+		assertNotNull(arguments);
+
+		assertEquals(arguments.size(), 2);
+
+		assertNotNull(arguments.get("properties.vessel.mmsi"));
+		assertNotNull(arguments.get("properties.date"));
+	}
+
+	@Test
+	public void sendVesselTrackingDeleteEvent_PublishDeleteVesselTrackingFailedEvent_IfNoConstraintsFulfilled()
+			throws Exception {
+
+		// TODO: Implementar cuando se metan las referencias en la vista.
+		assertTrue(true);
+	}
+
+	@KafkaHandler
+	public void createVesselTrackingConfirmed(CreateVesselTrackingConfirmedEvent createVesselTrackingConfirmedEvent) {
+
+		blockingQueue.offer(createVesselTrackingConfirmedEvent);
+	}
+
+	@KafkaHandler
+	public void createVesselTrackingFailed(CreateVesselTrackingFailedEvent createVesselTrackingFailedEvent) {
+
+		blockingQueue.offer(createVesselTrackingFailedEvent);
+	}
+
+	@KafkaHandler
+	public void updateVesselTrackingConfirmed(UpdateVesselTrackingConfirmedEvent updateVesselTrackingConfirmedEvent) {
+
+		blockingQueue.offer(updateVesselTrackingConfirmedEvent);
+	}
+
+	@KafkaHandler
+	public void updateVesselTrackingFailed(UpdateVesselTrackingFailedEvent updateVesselTrackingFailedEvent) {
+
+		blockingQueue.offer(updateVesselTrackingFailedEvent);
+	}
+
+	@KafkaHandler
+	public void deleteVesselTrackingConfirmed(DeleteVesselTrackingConfirmedEvent deleteVesselTrackingConfirmedEvent) {
+
+		blockingQueue.offer(deleteVesselTrackingConfirmedEvent);
+	}
+
+	@KafkaHandler
+	public void deleteVesselTrackingFailed(DeleteVesselTrackingFailedEvent deleteVesselTrackingFailedEvent) {
+
+		blockingQueue.offer(deleteVesselTrackingFailedEvent);
+	}
+
+	@KafkaHandler(isDefault = true)
+	public void defaultEvent(Object def) {
+		System.out.println("Entrando por default " + def);
+	}
+
+	protected VesselTrackingDTO getVesselTracking() {
+
+		VesselTrackingDTO vesselTracking = new VesselTrackingDTO();
+
+		VesselDTO vessel = new VesselDTO();
+		vessel.setId("ww1effbf-700f-4dfb-8aba-8212ac2d26b3");
+		vessel.setMmsi(1234);
+		vessel.setName("Avatar");
+		vessel.setImo(1234);
+		vessel.setCallSign("PACH");
+
+		VesselTypeDTO vesselType = new VesselTypeDTO();
+		vesselType.setCode("70");
+		vesselType.setId("1234");
+		vesselType.setName("Cargo, all ships of this type");
+		vesselType.setName_en("Cargo, all ships of this type");
+		vessel.setType(vesselType);
+
+		vesselTracking.setId(PREFIX + MMSI + TSTAMP);
+		vesselTracking.setUuid(_UUID);
+
+		Point geometry = JTSFactoryFinder.getGeometryFactory().createPoint(new Coordinate(44.56433, 37.94388));
+		vesselTracking.setGeometry(geometry);
+
+		VesselTrackingPropertiesDTO properties = new VesselTrackingPropertiesDTO();
+		vesselTracking.setProperties(properties);
+
+		properties.setActivity("r.1.8.22");
+		properties.setVessel(vessel);
+		properties.setDate(DateTime.now());
+
+		properties.setCog(23.3);
+		properties.setSog(23.3);
+		properties.setHeading(12);
+		properties.setNavStat(33);
+		properties.setDest("Santa Cruz de Tenerife");
+		properties.setEta("00:00 00:00");
+
+		return vesselTracking;
+	}
+
+	protected CreateVesselTrackingEvent getCreateVesselTrackingEvent() {
+
+		CreateVesselTrackingEvent createdEvent = new CreateVesselTrackingEvent();
+		createdEvent.setId(UUID.randomUUID().toString());
+		createdEvent.setDate(DateTime.now());
+		createdEvent.setType(VesselTrackingEventTypes.CREATE);
+		createdEvent.setVesselTracking(getVesselTracking());
+		createdEvent.setAggregateId(createdEvent.getVesselTracking().getId());
+		createdEvent.setVersion(1);
+		createdEvent.setSessionId(UUID.randomUUID().toString());
+		createdEvent.setUserId(USER_ID);
+		return createdEvent;
+	}
+
+	protected UpdateVesselTrackingEvent getUpdateVesselTrackingEvent() {
+
+		UpdateVesselTrackingEvent updatedEvent = new UpdateVesselTrackingEvent();
+		updatedEvent.setId(UUID.randomUUID().toString());
+		updatedEvent.setDate(DateTime.now());
+		updatedEvent.setType(VesselTrackingEventTypes.UPDATE);
+		VesselTrackingDTO vesselTracking = getVesselTracking();
+		vesselTracking.getProperties().setDest(vesselTracking.getProperties().getDest() + "2");
+		updatedEvent.setVesselTracking(vesselTracking);
+		updatedEvent.setAggregateId(updatedEvent.getVesselTracking().getId());
+		updatedEvent.setVersion(2);
+		updatedEvent.setSessionId(UUID.randomUUID().toString());
+		updatedEvent.setUserId(USER_ID);
+		return updatedEvent;
+	}
+
+	protected UpdateVesselInVesselTrackingEvent getUpdateVesselInVesselTrackingEvent() {
+
+		UpdateVesselInVesselTrackingEvent event = new UpdateVesselInVesselTrackingEvent()
+				.buildFrom(getUpdateVesselTrackingEvent());
+
+		VesselTrackingDTO vesselTracking = getVesselTracking();
+		vesselTracking.getProperties().getVessel().setName("Nombre cambiado");
+		event.setVessel(vesselTracking.getProperties().getVessel());
+		event.setAggregateId(vesselTracking.getId());
+		event.setVersion(3);
+		event.setSessionId(UUID.randomUUID().toString());
+		event.setUserId(USER_ID);
+
+		return event;
+	}
+
+	protected DeleteVesselTrackingEvent getDeleteVesselTrackingEvent() {
+
+		DeleteVesselTrackingEvent deletedEvent = new DeleteVesselTrackingEvent();
+		deletedEvent.setId(UUID.randomUUID().toString());
+		deletedEvent.setDate(DateTime.now());
+		deletedEvent.setType(VesselTrackingEventTypes.DELETE);
+		deletedEvent.setAggregateId(getVesselTracking().getId());
+		deletedEvent.setVersion(3);
+		deletedEvent.setSessionId(UUID.randomUUID().toString());
+		deletedEvent.setUserId(USER_ID);
+		return deletedEvent;
+	}
+
+	private void assertEqualsVesselTracking(VesselTracking vesselTracking, VesselTrackingDTO vesselTrackingDTO)
+			throws JsonProcessingException, JSONException {
+
+		JSONAssert.assertEquals(objectMapper.writeValueAsString(vesselTracking),
+				objectMapper.writeValueAsString(vesselTrackingDTO), false);
+	}
+}
