c5cdaf1c45766ef1b2d55f47632a181ababa394f
Noel Alonso <noel@redmic.es>
2018-10-31 10:25:41 +0000

    Añade tag a métricas de prometheus

--- vessels-commands/src/main/java/es/redmic/vesselscommands/VesselsCommandsApplication.java
+++ vessels-commands/src/main/java/es/redmic/vesselscommands/VesselsCommandsApplication.java
@@ -1,6 +1,8 @@
 package es.redmic.vesselscommands;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +13,15 @@ import com.fasterxml.jackson.databind.Module;
 
 import es.redmic.commandslib.config.GenerateJsonSchemaScanBean;
 import es.redmic.restlib.config.ResourceBundleMessageSource;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @ComponentScan({ "es.redmic.vesselscommands", "es.redmic.restlib", "es.redmic.commandslib",
 		"es.redmic.brokerlib.alert" })
 public class VesselsCommandsApplication {
+
+	@Value("${info.microservice.name}")
+	String microserviceName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VesselsCommandsApplication.class, args);
@@ -35,5 +41,10 @@ public class VesselsCommandsApplication {
 	@Bean
 	public Module jtsModule() {
 		return new JtsModule();
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", microserviceName);
 	}
 }
