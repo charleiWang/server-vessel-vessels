f9c2f979ad44561c45e0ac973b6f097f2dbe0a19
Noel Alonso <noel@redmic.es>
2018-11-28 14:06:07 +0000

    Añade paso de aisTracking a vessel y tracking
    Añade test de esa utilidad

--- vessels-lib/src/main/java/es/redmic/vesselslib/utils/VesselTrackingUtil.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/utils/VesselTrackingUtil.java
@@ -1,10 +1,59 @@
 package es.redmic.vesselslib.utils;
 
+import org.geotools.geometry.jts.JTSFactoryFinder;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+import es.redmic.exception.databinding.FieldNotValidException;
+import es.redmic.vesselslib.dto.ais.AISTrackingDTO;
+import es.redmic.vesselslib.dto.tracking.VesselTrackingDTO;
+import es.redmic.vesselslib.dto.tracking.VesselTrackingPropertiesDTO;
+
 public class VesselTrackingUtil {
 
 	private static final String PREFIX = "vesseltracking-mmsi-tstamp-";
 
 	public static String generateId(Integer mmsi, Long timeStamp) {
 		return PREFIX + mmsi + "-" + timeStamp;
+	}
+
+	public static VesselTrackingDTO convertTrackToVesselTracking(AISTrackingDTO aisTracking, String qFlagDefault,
+			String vFlagDefault) {
+
+		if (aisTracking.getMmsi() == null)
+			throw new FieldNotValidException("mmsi", "null");
+
+		if (aisTracking.getTstamp() == null)
+			throw new FieldNotValidException("date", "null");
+
+		GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+		VesselTrackingDTO vesselTracking = new VesselTrackingDTO();
+
+		Point geometry = geometryFactory
+				.createPoint(new Coordinate(aisTracking.getLongitude(), aisTracking.getLatitude()));
+
+		vesselTracking.setGeometry(geometry);
+
+		VesselTrackingPropertiesDTO properties = new VesselTrackingPropertiesDTO();
+
+		properties.setVessel(VesselUtil.convertTrackToVessel(aisTracking));
+
+		properties.setDate(aisTracking.getTstamp());
+
+		properties.setCog(aisTracking.getCog());
+		properties.setSog(aisTracking.getSog());
+		properties.setHeading(aisTracking.getHeading());
+		properties.setNavStat(aisTracking.getNavStat());
+		properties.setDest(aisTracking.getDest());
+		properties.setEta(aisTracking.getEta());
+		properties.setQFlag(qFlagDefault);
+		properties.setVFlag(vFlagDefault);
+
+		vesselTracking.setProperties(properties);
+
+		return vesselTracking;
 	}
 }
--- vessels-lib/src/main/java/es/redmic/vesselslib/utils/VesselUtil.java
+++ vessels-lib/src/main/java/es/redmic/vesselslib/utils/VesselUtil.java
@@ -1,10 +1,36 @@
 package es.redmic.vesselslib.utils;
 
+import es.redmic.vesselslib.dto.ais.AISTrackingDTO;
+import es.redmic.vesselslib.dto.vessel.VesselDTO;
+import es.redmic.vesselslib.dto.vesseltype.VesselTypeDTO;
+
 public class VesselUtil {
 
 	private static final String PREFIX = "vessel-mmsi-";
 
 	public static String generateId(Integer mmsi) {
 		return PREFIX + mmsi;
+	}
+
+	public static VesselDTO convertTrackToVessel(AISTrackingDTO aisTracking) {
+
+		VesselDTO vessel = new VesselDTO();
+
+		vessel.setMmsi(aisTracking.getMmsi());
+		vessel.setName(aisTracking.getName());
+		vessel.setCallSign(aisTracking.getCallSign());
+		vessel.setImo(aisTracking.getImo());
+
+		if (aisTracking.getA() != null && aisTracking.getB() != null)
+			vessel.setLength(aisTracking.getA() + aisTracking.getB());
+
+		if (aisTracking.getC() != null && aisTracking.getD() != null)
+			vessel.setBeam(aisTracking.getC() + aisTracking.getD());
+
+		VesselTypeDTO vesselType = new VesselTypeDTO();
+		vesselType.setCode(aisTracking.getType().toString());
+		vessel.setType(vesselType);
+
+		return vessel;
 	}
 }
