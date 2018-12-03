831c10d01d821787c4e83805165265a350bcc27f
Noel Alonso <noel@redmic.es>
2018-12-03 11:56:43 +0000

    Elimina listener para crear track vía command

--- vessels-commands/src/main/java/es/redmic/vesselscommands/controller/VesselTrackingController.java
+++ vessels-commands/src/main/java/es/redmic/vesselscommands/controller/VesselTrackingController.java
@@ -1,8 +1,6 @@
 package es.redmic.vesselscommands.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,21 +12,8 @@ import es.redmic.vesselslib.dto.tracking.VesselTrackingDTO;
 @RequestMapping(value = "${controller.mapping.vesseltracking}")
 public class VesselTrackingController extends CommandGeoController<VesselTrackingDTO> {
 
-	VesselTrackingCommandService service;
-
-	@Value("${vesseltracking-activity-id}")
-	protected String activityId;
-
 	@Autowired
 	public VesselTrackingController(VesselTrackingCommandService service) {
 		super(service);
-		this.service = service;
-	}
-
-	@KafkaListener(topics = "${broker.topic.realtime.tracking.vessels}")
-	public void run(VesselTrackingDTO dto) throws InterruptedException {
-
-		logger.info("Procesando track para el barco: " + dto.getId() + " date: " + dto.getProperties().getDate());
-		service.create(dto, activityId);
 	}
 }
