310a7078cbf4b17bc2a6d4245e3ee5e248d64707
Noel Alonso <noel@redmic.es>
2018-12-14 10:15:12 +0000

    Cambia módulo JTS para jackson

--- vessels-view/src/main/java/es/redmic/vesselsview/VesselsViewApplication.java
+++ vessels-view/src/main/java/es/redmic/vesselsview/VesselsViewApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.bedatadriven.jackson.datatype.jts.JtsModule;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -17,6 +16,7 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
+import es.redmic.jts4jackson.module.JTSModule;
 import es.redmic.models.es.common.view.QueryDTODeserializerModifier;
 import es.redmic.restlib.common.service.UserUtilsServiceItfc;
 import es.redmic.restlib.config.ResourceBundleMessageSource;
@@ -49,7 +49,7 @@ public class VesselsViewApplication {
 
 	@Bean
 	public Module jtsModule() {
-		return new JtsModule();
+		return new JTSModule();
 	}
 
 	@Bean
